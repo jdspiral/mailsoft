76b8da7318c1a5f59eca29b6cb93669d4886b653
jdspiral <jdspiral@gmail.com>
2016-02-24 22:27:52 -0500

    Remove Mailchimp service provider

--- config/app.php
+++ config/app.php
@@ -147,7 +147,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Skovmand\Mailchimp\MailchimpServiceProvider::class,
 
         /*
          * Application Service Providers...
