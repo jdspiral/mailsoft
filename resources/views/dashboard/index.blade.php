5735e92103714bc3b679310eacd85fa46b08c157
jdspiral <jdspiral@gmail.com>
2016-02-25 22:55:25 -0500

    Update dashboard verbiage

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,8 +7,10 @@
         @if($count == 0)
             <p>You have no unsynced contacts.</p>
         @else
-            <p>You have {{ $count }} unsynced contacts.</p>
-            <p>To sync your contacts <button  href="{{ url('/contacts') }}" class="btn btn-default">click here</button></p>
+            <p>You have {{ $count }} contacts in Mailchimp that can be added to Infusionsoft.</p>
+            <p>To import your contacts <button  href="{{ url('/contacts') }}" class="btn btn-default">click here</button></p>
+            <p>If you would like to tag your contacts before import, please select which tags you would like to add.</p>
+            @include('partials.tags')
         @endif
     @endif
 
